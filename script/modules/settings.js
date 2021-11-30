7f817be192a107613f67249851d55e26785fe524
AnnaCybulska <anna357y@stud.kea.dk>
2021-11-30 20:51:54 +0100

    Trying out- showing queue

--- script/modules/settings.js
+++ script/modules/settings.js
@@ -1,2 +1,6 @@
-export const url =
-  "https://www.google.com/url?q=https://foo-bar-database.herokuapp.com/&sa=D&source=docs&ust=1638289289719000&usg=AOvVaw3Jv-_Zhi1l5MD5eOO7O2aP";
+const orders_api = "https://foo-bar-database.herokuapp.com/";
+const beer_api = "https://foo-bar-database.herokuapp.com/beertypes";
+
+const refresh_rate = 3000; // in miliseconds
+
+export { orders_api, beer_api, refresh_rate };
