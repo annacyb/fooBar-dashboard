97ad2af2a8162a033d40b98561ffad049ff0c175
AnnaCybulska <anna357y@stud.kea.dk>
2021-12-02 16:06:06 +0100

    Closing time calculation made

--- script/modules/time-counting.js
+++ script/modules/time-counting.js
@@ -5,10 +5,12 @@ async function getCurrentTime() {
     let ordersData = await get_orders();
     let timestamp = ordersData.timestamp;
 
-    let time = dayjs(timestamp).format("HH:mm:ss");
+    let time = dayjs(timestamp);
 
     // showing time for each element that has class "current-time-place"
-    showTimeOnPage(time);
+    showTimeOnPage(time.format("HH:mm:ss"));
+
+    showTimeTillClosing(ordersData, time);
 }
 
 function showTimeOnPage(time) {
@@ -19,6 +21,32 @@ function showTimeOnPage(time) {
         element.innerHTML = "";
         element.innerHTML = time;
     });
+}
+
+function showTimeTillClosing(ordersData, currentTime) {
+    // get closing time from database (it is only hour not timestamp)
+    const closingHour = ordersData.bar.closingTime;
+    const dateNow = dayjs().format("MM-DD-YYYY");
+    console.log("now", dateNow);
+    const closingTime = dayjs(
+        `${dateNow} ${closingHour}`,
+        "MM-DD-YYYY H:mm:SS"
+    ); // library works with USA date system
+
+    console.log(closingTime);
+    console.log(currentTime);
+
+    // calculating inspired by:
+    // https://stackoverflow.com/questions/24936855/get-time-difference-in-hours-minutes-and-seconds
+
+    let diff = closingTime.diff(currentTime, "second");
+    let hours = Math.floor(diff / 3600);
+    let minute = Math.floor((diff - hours * 3600) / 60);
+    let seconds = Math.floor(diff - hours * 3600 - minute * 60);
+
+    console.log("CHECK ", hours, minute, seconds);
+
+    // show in HTML
 }
 
 setInterval(getCurrentTime, 1000);
