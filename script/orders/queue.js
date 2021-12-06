a0edb3a0630cfb2dc0e29001122e540d38503cb8
AnnaCybulska <anna357y@stud.kea.dk>
2021-12-06 20:43:11 +0100

    Order details appended to Jonas for now, checking color circle change for beers, added json file with main colors of beers

--- script/orders/queue.js
+++ script/orders/queue.js
@@ -1,6 +1,6 @@
 import { changeTimestampToTime } from "../modules/time-counting.js";
 
-const queueContainer = document.querySelector("#queue-orders");
+const queueContainer = document.querySelector("#queue-orders-place");
 
 function clearQueue() {
     queueContainer.innerHTML = "";
@@ -13,22 +13,19 @@ function setupEventListener(container) {
     });
 }
 
-function showData(data) {
+function showQueueData(data) {
     let countOrders = 0;
 
     data.orders.queue.forEach((element) => {
-        // create some template etc... and append to queueContainer
-        // let queue_template;
-
-        //grab the template
+        //grab the template for order in a queue
         const queue_order_template = document.querySelector(
             "template.queue-order-template"
         ).content;
 
         //clone it
         const myCopy = queue_order_template.cloneNode(true);
-        //change content
 
+        //change content
         myCopy.querySelector(".order-id").textContent = "#" + element.id;
 
         const orderTimestamp = element.startTime;
@@ -37,26 +34,61 @@ function showData(data) {
 
         // myCopy.querySelector(".order-details-row-name").src = element[0];
 
-        element.order.forEach((order) => {
-            console.log("ORDER ", order);
+        element.order.forEach((orderName) => {
+            const random = Math.floor(Math.random() * 100) + 1;
+            console.log("ORDER ", random, orderName);
+
+            //grab the template for order details
+            const queue_order_details_template = document.querySelector(
+                "template#queue-order-details-template"
+            ).content;
+
+            //clone it
+            const orderDetailsCopy =
+                queue_order_details_template.cloneNode(true);
+
+            //change content
+
+            // TO DO
+            //setting colour of circles for every beer
+            const beerColor = setBeerMainColor(orderName);
+
+            orderDetailsCopy.querySelector(
+                ".order-details-row-color"
+            ).style.backgroundColor = "red";
+
+            //TO DO
+            // orderDetailsCopy.querySelector(
+            //     ".order-details-row-color"
+            // ).style.backgroundColor = beerColor;
+
+            orderDetailsCopy.querySelector(
+                ".order-details-row-name"
+            ).textContent = orderName;
+
+            //grab parent
+            // const orderContainer = document.querySelector(
+            //     ".order-details-place"
+            // );
+
+            // CHECKING IF TEMPLATE IS APPENDING SOMEWHERE
+            const orderContainer = document.querySelector("#bartender-jonas");
+
+            //append order details
+            orderContainer.appendChild(orderDetailsCopy);
+
             // TO DO
             // count the same beers
         });
 
-        // TO DO AFTER
-        //setting colour of circles for every beer
-        // setCirclesForBeers(data, myCopy);
-
         //grab parent
-        const parent = document.querySelector("#queue-orders");
-        // console.log(parent)
+        const parent = document.querySelector("#queue-orders-place");
 
         //append
         parent.appendChild(myCopy);
 
-        // setupEventListener(queue_template)
-        // setting event listeners on templates won't work unfortunately (it must be DOM object not Template object)
-        // solution? create new loop on queueContainer childrens and add event listeners there
+        // const orderContainer = document.querySelector(".order-details-place");
+        // orderContainer.appendChild(orderDetailsCopy);
     });
     showOrderNr(countOrders);
 }
@@ -66,7 +98,7 @@ function showQueue(data) {
     if (data.orders.queue.length == 0) {
         showMissingData();
     } else {
-        showData(data);
+        showQueueData(data);
     }
 }
 
@@ -78,6 +110,11 @@ function showMissingData() {
 function showOrderNr(number) {
     const ordersNrCont = document.querySelector("#queue-nr");
     ordersNrCont.innerHTML = number;
+}
+
+function setBeerMainColor(beerName) {
+    // TO DO
+    // return BeerColor;
 }
 
 export { showQueue };
