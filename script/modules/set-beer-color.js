a040b0b153f234d57b1c5e7c07a5b3d48e350bb2
Julia Lendzion <juli47g3@stud.kea.dk>
2021-12-14 12:46:45 +0100

    json

--- script/modules/set-beer-color.js
+++ script/modules/set-beer-color.js
@@ -1,28 +1,28 @@
 async function getBeerMainColors() {
-    const response = await fetch("../beer-colors.json");
-    return await response.json();
+  const response = await fetch("././beer-colors.json");
+  return await response.json();
 }
 
 export async function setBeerMainColor(beerName) {
-    // fetch data
-    const dataWithColors = await getBeerMainColors();
+  // fetch data
+  const dataWithColors = await getBeerMainColors();
 
-    // find the same beer name in JSON file and find it's color
-    let found = dataWithColors.beers.filter((beer) => beer.name == beerName);
-    if (found.length == 0) {
-        console.log("Beer not found");
-    }
-    return found[0].color;
+  // find the same beer name in JSON file and find it's color
+  let found = dataWithColors.beers.filter((beer) => beer.name == beerName);
+  if (found.length == 0) {
+    console.log("Beer not found");
+  }
+  return found[0].color;
 }
 
 export async function setBeerGradient(beerName) {
-    // fetch data
-    const dataWithColors = await getBeerMainColors();
+  // fetch data
+  const dataWithColors = await getBeerMainColors();
 
-    // find the same beer name in JSON file and find it's color
-    let found = dataWithColors.beers.filter((beer) => beer.name == beerName);
-    if (found.length == 0) {
-        console.log("Beer not found");
-    }
-    return found[0].gradient;
+  // find the same beer name in JSON file and find it's color
+  let found = dataWithColors.beers.filter((beer) => beer.name == beerName);
+  if (found.length == 0) {
+    console.log("Beer not found");
+  }
+  return found[0].gradient;
 }
